0904a746b4f520cd55b5e6e11b13182111ed6c9b
Chaow Porkaew <pchaowmobile@gmail.com>
2022-01-27 16:23:53 +0700

    update

---
+++ docs/precache-manifest.f40355e0e8c2a828e4ee5e7489cb0a68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750b6dd0025809758202365fe5717d1e",
+    "url": "/OfficeSyndromeHelper/audio/resting_timeout.mp3"
+  },
+  {
+    "revision": "4c563e35b45213d3f6fd2e47e77ea164",
+    "url": "/OfficeSyndromeHelper/audio/start_resting.mp3"
+  },
+  {
+    "revision": "a8b087b38e4a90681eb178695f13c3df",
+    "url": "/OfficeSyndromeHelper/audio/start_working.mp3"
+  },
+  {
+    "revision": "b12f20abf25cea2741dd5b45a0559893",
+    "url": "/OfficeSyndromeHelper/audio/working_timeout.mp3"
+  },
+  {
+    "revision": "658f64011f94015afcab",
+    "url": "/OfficeSyndromeHelper/css/chunk-vendors.085711c5.css"
+  },
+  {
+    "revision": "5c2c62d4c1a815b23a6fd1c8e4b2cdf6",
+    "url": "/OfficeSyndromeHelper/index.html"
+  },
+  {
+    "revision": "3d658947c9710e57627b",
+    "url": "/OfficeSyndromeHelper/js/app.bab6b0e9.js"
+  },
+  {
+    "revision": "658f64011f94015afcab",
+    "url": "/OfficeSyndromeHelper/js/chunk-vendors.4b684765.js"
+  },
+  {
+    "revision": "397f99696b3640c090f327b93d31406a",
+    "url": "/OfficeSyndromeHelper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/OfficeSyndromeHelper/robots.txt"
+  }
+]);
